400dc1c57cad1a9e87e0f5b78ecab05c32faef56
Diontre Sanders <118845810+houseofpython@users.noreply.github.com>
2023-06-04 22:54:33 -0500

    Create stack-and-queue.py

---
+++ python/stack-and-queue.py
@@ -0,0 +1,49 @@
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+
+    def pop(self):
+        if self.is_empty():
+            return "Stack is empty"
+        value = self.top.value
+        self.top = self.top.next
+        return value
+
+    def peek(self):
+        if self.is_empty():
+            return "Stack is empty"
+        return self.top.value
+
+    def is_empty(self):
+        return self.top is None
+
+
+class Queue:
+    def __init__(self):
+        self.front = None
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.front is None:
+            self.front = new_node
+        else:
+            current = self.front
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+
+    def dequeue(self):
+        if self.is_empty():
+            return "Queue is empty"
+        value = self.front.value
+        self.front = self.front.next
+        return value
+
+    def peek(self):
