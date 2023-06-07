9e4e5e75c27a71827e3baae2976fab311045e610
‘Diontre <diontre01@gmail.com>
2023-06-07 00:48:18 -0500

    binary

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,15 +1,37 @@
 from data_structures.binary_tree import BinaryTree
-
+from data_structures.binary_tree import Node
 
 class BinarySearchTree(BinaryTree):
-    """
-    Put docstring here
-    """
-
     def __init__(self):
-        # initialization here
-        pass
+        super().__init__()
 
-    def some_method(self):
-        # method body here
-        pass
+    def add(self, value):
+        def insert(node, value):
+            if value < node.value:
+                if node.left is None:
+                    node.left = Node(value)
+                else:
+                    insert(node.left, value)
+            else:
+                if node.right is None:
+                    node.right = Node(value)
+                else:
+                    insert(node.right, value)
+
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            insert(self.root, value)
+
+    def contains(self, value):
+        def search(node, value):
+            if node is None:
+                return False
+            elif node.value == value:
+                return True
+            elif value < node.value:
+                return search(node.left, value)
+            else:
+                return search(node.right, value)
+
+        return search(self.root, value)
--- python/data_structures/binary_tree.py
+++ python/data_structures/binary_tree.py
@@ -1,16 +1,43 @@
-class BinaryTree:
-    """
-    Put docstring here
-    """
-
-    def __init__(self):
-        # initialization here
-        pass
-
-    def some_method(self):
-        # method body here
-        pass
-
-
 class Node:
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def pre_order(self):
+        def traverse(node, result):
+            if node:
+                result.append(node.value)
+                traverse(node.left, result)
+                traverse(node.right, result)
+
+        result = []
+        traverse(self.root, result)
+        return result
+
+    def in_order(self):
+        def traverse(node, result):
+            if node:
+                traverse(node.left, result)
+                result.append(node.value)
+                traverse(node.right, result)
+
+        result = []
+        traverse(self.root, result)
+        return result
+
+    def post_order(self):
+        def traverse(node, result):
+            if node:
+                traverse(node.left, result)
+                traverse(node.right, result)
+                result.append(node.value)
+
+        result = []
+        traverse(self.root, result)
+        return result
