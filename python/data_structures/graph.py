243757320f55e2b1692b3d01ef33bd134012ca9b
Diontre Sanders <118845810+houseofpython@users.noreply.github.com>
2023-06-09 14:14:57 -0500

    Update graph.py

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -1,31 +1,14 @@
 class Graph:
 
-    def _init__(self):
+    def __init__(self):
         self.adjacency_list = {}
 
     def add_vertex(self, vertex):
         self.adjacency_list[vertex] = []
 
-        """
-        add vertex
-        Arguments: value
-        Add a vertx to the graph
-        :return: added vertex
-        """
-
     def add_edge(self, vertex1, vertex2):
         self.adjacency_list[vertex1].append(vertex2)
         self.adjacency_list[vertex2].append(vertex1)
-
-        """
-        add edge
-        Arguments: 2 vertex to be connected by the edge, weight (optional)
-        Returns: nothing
-        Adds a new edge between two vertex in the graph
-        If specified, assign a weight to the edge
-        Both vertex should already be in the Graph
-        :return:
-        """
 
     def get_node(self, vertex):
         if vertex in self.adjacency_list:
@@ -39,9 +22,11 @@ class Graph:
     def size(self):
         return len(self.adjacency_list)
 
+
 class Vertex:
-    def __init(self, value):
+    def __init__(self, value):
         self.value = value
+
 
 class Edge:
     def __init__(self, vertex, weight=1):
