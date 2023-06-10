0bbdee347dfd6276f182019d9e8d2818f3f31719
Diontre Sanders <118845810+houseofpython@users.noreply.github.com>
2023-06-10 01:14:12 -0500

    Create test_graph_depth_first_search.py

---
+++ python/data_structures/test_graph_depth_first_search.py
@@ -0,0 +1,24 @@
+import pytest
+
+@pytest.fixture
+def graph():
+    g = Graph()
+    g.add_node('A')
+    g.add_node('B')
+    g.add_node('C')
+    g.add_node('D')
+
+    g.add_edge('A', 'B')
+    g.add_edge('A', 'C')
+    g.add_edge('B', 'D')
+    g.add_edge('C', 'D')
+
+    return g
+
+def test_depth_first(graph):
+    start_node = 'A'
+    expected = ['A', 'B', 'D', 'C']
+    actual = graph.depth_first(start_node)
+    assert actual == expected
+
+pytest.main()
